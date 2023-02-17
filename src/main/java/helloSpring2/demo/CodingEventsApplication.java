671bd680962c3a0ef79fe98087f0a718c5f4146f
AJmints <alexjmints@gmail.com>
2023-02-17 17:24:29 -0600

    initial commit, prebootstrap

---
+++ src/main/java/helloSpring2/demo/CodingEventsApplication.java
@@ -0,0 +1,13 @@
+package helloSpring2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingEventsApplication.class, args);
+	}
+
+}
